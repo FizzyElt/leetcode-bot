da996f5c5823dcf27ddf05553343ab9ab33d7607
FizzyElt <fizzyelt.dev@outlook.com>
2024-01-18 21:13:58 +0800

    update package

--- utils/command_option.ts
+++ utils/command_option.ts
@@ -1,24 +1,30 @@
-import { ApplicationCommandOptionType, CacheType, CommandInteraction } from 'discord.js';
-import { pipe, Option, ReadonlyArray, Function } from 'effect';
-import * as R from 'remeda';
+import {
+	ApplicationCommandOptionType,
+	CacheType,
+	CommandInteraction,
+} from "discord.js";
+import { pipe, Option, ReadonlyArray } from "effect";
+import * as Function from "effect/Function";
+import * as String from "effect/String";
+import { equals } from "effect/Equal";
 
 export const getCommandOptionOfType =
-  (type: ApplicationCommandOptionType, optionName: string) =>
-  (interaction: CommandInteraction<CacheType>) =>
-    pipe(
-      interaction.options.data,
-      ReadonlyArray.findFirst((option) => R.equals(option.name, optionName)),
-      Option.filter((option) => R.equals(option.type, type))
-    );
+	(type: ApplicationCommandOptionType, optionName: string) =>
+	(interaction: CommandInteraction<CacheType>) =>
+		pipe(
+			interaction.options.data,
+			ReadonlyArray.findFirst((option) => equals(option.name, optionName)),
+			Option.filter((option) => equals(option.type, type)),
+		);
 
 export const getCommandOptionString =
-  (optionName: string) =>
-  (interaction: CommandInteraction<CacheType>): string => {
-    return pipe(
-      interaction,
-      getCommandOptionOfType(ApplicationCommandOptionType.String, optionName),
-      Option.map(R.prop('value')),
-      Option.filter(R.isString),
-      Option.getOrElse(Function.constant(''))
-    );
-  };
+	(optionName: string) =>
+	(interaction: CommandInteraction<CacheType>): string => {
+		return pipe(
+			interaction,
+			getCommandOptionOfType(ApplicationCommandOptionType.String, optionName),
+			Option.map((option) => option.value),
+			Option.filter(String.isString),
+			Option.getOrElse(Function.constant("")),
+		);
+	};
